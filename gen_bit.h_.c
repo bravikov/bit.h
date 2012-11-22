602cd9ff4801f6f87ff72d89f9d4c3c28fe561fe
Dmitry Bravikov <bravikov@gmail.com>
2012-11-22 20:41:59 +0600

    Добавлено разделение тетрад

--- gen_bit.h_.c
+++ gen_bit.h_.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-// make: gcc -std=c99 gen_bit.h_.c
+//        сборка: gcc -std=c99 gen_bit.h_.c
+// использование: ./a.out > bit.h
 
 int main()
 {   
+    printf("#ifndef BIT_H\n");
+    printf("#define BIT_H\n\n");
+    
+    // Без разделения тетрад
     for (int i = 0; i < 256; i++)
     {
         printf("#define b");
@@ -18,6 +23,28 @@ int main()
         
         printf(" %i\n",i);
     }
+    
+    printf("\n");
+    
+    // С разделением тетрад
+    for (int i = 0; i < 256; i++)
+    {
+        printf("#define b");
+        
+        for (int b = 7; b >= 0; b--)
+        {
+            if ( i & (1 << b) )
+                printf("1");
+            else
+                printf("0");
+            
+            if (b == 4) printf("_");
+        }
+        
+        printf(" %i\n",i);
+    }
+    
+    printf("\n#endif\n\n");
     
     return 0;
 }
