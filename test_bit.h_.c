89632c38455ee9e1cd1a1bf84b052feb62a855f8
Dmitry Bravikov <bravikov@gmail.com>
2012-11-23 21:44:12 +0600

    Добавлен забытоый исходник тестировочной программы

---
+++ test_bit.h_.c
@@ -0,0 +1,23 @@
+// project: https://github.com/bravikov/bit.h
+//  author: Dmitry Bravikov (bravikov@gmail.com)
+
+//     use: gcc -Wall -std=c99 -o test test_bit.h_.c && ./test
+
+#include <stdio.h>
+#include <stdint.h>
+#include "bit.h"
+
+int main()
+{
+    uint32_t test1 = 0xFFFFFFFF;
+    uint32_t test2 = b1111_1111_3 | b1111_1111_2 | b1111_1111_1 | b1111_1111;
+    
+    printf("Test bit.h ... ");
+    if (test1 == test2)
+        printf("OK\n");
+    else
+        printf("fail (%u)\n", test2);
+        
+    return 0;
+}
+
